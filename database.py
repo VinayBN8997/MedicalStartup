1e3d539724be266318dd802ccf6b31c13bdcc2d6
Bn <bnvina@186590d222b9.ant.amazon.com>
2019-11-16 19:07:39 +0530

    sqlalchemy

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 from app import db
 
 class User(db.Model):
-    email = db.Column(db.String(100), uqique = True, nullable = False)
-    username = db.Column(db.String(100), uqique = True, nullable = False)
+    email = db.Column(db.String(100), unique = True, nullable = False)
+    username = db.Column(db.String(100), unique = True, nullable = False)
     password = db.Column(db.String(100), nullable = False)
 
     def __repr__(self):
